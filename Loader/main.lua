c30f63796310f701b7232dd030f253d6b9888bd0
putraborz <putra.myapi@gmail.com>
2025-10-31 16:03:00 +0700

    Update main.lua

--- Loader/main.lua
+++ Loader/main.lua
@@ -16,7 +16,7 @@ local urlSatuan = "https://raw.githubusercontent.com/putraborz/VerifikasiScWata/
 
 local successUrls = {
     "https://raw.githubusercontent.com/putraborz/WataXMountAtin/main/Loader/WataX.lua",
-    "/WataXMountYahayuk1/refs/heads/main/Loader/mainmap970.lua"
+    "https://raw.githubusercontent.com/putraborz/WataXMountYahayuk1/refs/heads/main/Loader/mainmap970.lua"
 }
 
 -- helper fetch yg lebih toleran (pcall sudah ada tapi jaga lagi)
